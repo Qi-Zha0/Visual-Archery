91f1852cc0999bbe13f4199f62e464d7b4a4117b
Qi-Zha0 <mz2651@columbia.edu>
2021-02-10 12:51:21 -0500

    updated examples

--- examples/example.py
+++ examples/example.py
@@ -1,4 +1,5 @@
 import sys
+import matplotlib.pyplot as plt
 
 sys.path.append('../code')
 from target_reader import target_reader
@@ -7,6 +8,10 @@ sample_file = 'example_target.jpg'
 
 tr = target_reader()
 result = tr.run(sample_file)
+output_image = tr.stage_images[-1]
+print('Output size:', output_image.shape)
+plt.imsave('example_output.jpg', output_image)
+
 if result:
     print('Failed:', result)
 else:
